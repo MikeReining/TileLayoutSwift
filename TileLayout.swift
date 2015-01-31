f1852e2f311846c34197611a46e30c3fa044e251
Mike Reining <emailmike@gmail.com>
2015-01-31 08:02:54 -0800

    TileLayoutSwift setup

---
+++ TileLayout.swift
@@ -0,0 +1,17 @@
+//
+//  TileLayout.swift
+//  TileLayoutSwift
+//
+//  Created by Michael Reining on 1/31/15.
+//  Copyright (c) 2015 Mike Reining. All rights reserved.
+//
+
+import UIKit
+
+protocol TileLayoutDelegate {
+    func heightForItemAtIndexPath(tileLayout: TileLayout, indexPath: NSIndexPath) -> CGFloat
+}
+
+class TileLayout: UICollectionViewLayout {
+   
+}
---
+++ TileLayoutSwift/MyPlayground.playground/section-1.swift
@@ -0,0 +1,22 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var double = CGFloat(80 + (arc4random() % 50))
+
+
+var myFloat: CGFloat
+
+myFloat = double
+
+
+
+
+
+
+
+
+
+
+
+
